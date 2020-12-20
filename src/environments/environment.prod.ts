6f7c3eb42c57596676a07357ecd8d4c167d59034
Andrew <andrew.haseo@gmail.com>
2020-12-20 11:00:41 +0700

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl:'https://ess-skripsi.herokuapp.com',
+    baseUrl:'localhost:8080',
     mapApiKey : "AIzaSyBgLAsvhRh_2N4JnDl6t9bE9dE3HZ64aDw",
 };
